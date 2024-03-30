b9c49f5f44036af60e01279fa6ce6f6445ee2ab0
mplicinski <mplicinski@gmail.com>
2024-03-30 17:53:41 -0500

    updated metadata & added custom favicon

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#151515]">
+      <title>mzp.dev</title>
       <Navbar/>
       <div className="container mt-24 mx-auto px-6 py-4">
         <HeroSection/>
